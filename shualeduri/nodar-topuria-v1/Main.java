5640780498f04e798d0ab27f2a86052648d60c6d
Topuria <36174597+topuria@users.noreply.github.com>
2020-11-11 19:41:35 +0400

    Update Main.java

--- shualeduri/nodar-topuria-v1/Main.java
+++ shualeduri/nodar-topuria-v1/Main.java
@@ -1,14 +1,21 @@
 package ge.edu.CurrencyConverter
 
+@Slf4j
 public class CurrencyConverter
 {
   
   public static void main(String args[])
   {
+      Scanner input = new Scanner(System.in);
+      System.out.println("შეიყვანე შენთვის სასურველი კურსი აქ მაინც :( ");
+      double r = input.nextDouble();
       CurrencyServiceImpl c = new CurrencyServiceImpl();
-      c.setExchangeRate(3.5);
-      c.convert(2);
-      c.convert(3.5);      
+      c.setExchangeRate(r);
+      log.info("შენთვის სასურველი კურსი: "+r);
+      int amount = c.convert(2);
+      double amount2 = c.convert(3.5);   
+      System.out.println(amount);
+      System.out.println(amount2);
     
   }
   
