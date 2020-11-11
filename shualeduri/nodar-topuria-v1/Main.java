86f1f55344dc9e6eb2038ee54dfdd9ed0379506b
Topuria <36174597+topuria@users.noreply.github.com>
2020-11-11 19:24:44 +0400

    Update Main.java

--- shualeduri/nodar-topuria-v1/Main.java
+++ shualeduri/nodar-topuria-v1/Main.java
@@ -1,1 +1,16 @@
+package ge.edu.CurrencyConverter
 
+public class CurrencyConverter
+{
+  
+  public static void main(String args[])
+  {
+      CurrencyServiceImpl c = new CurrencyServiceImpl();
+      c.setExchangeRate(3.5);
+      c.convert(2);
+      c.convert(3.5);      
+    
+  }
+  
+  
+}
